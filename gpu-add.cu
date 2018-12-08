34dc2fad2271ab3646da5fd69990a448dbcc67e2
liuzzom <33182647+liuzzom@users.noreply.github.com>
2018-12-08 17:18:29 +0100

    updated gpu-add.cu

--- gpu-add.cu
+++ gpu-add.cu
@@ -11,39 +11,82 @@
 #include <stdlib.h>
 
 #include <cuda.h>
+#define N 512
+
+void fillArray(int *array, int size){
+	int i;
+	for(i = 0; i < size; i++){
+		array[i] = 1;
+	}
+}
+
+void printInFile(int *array, int size){
+	int i;
+	FILE *fPtr;
+
+	if((fPtr = fopen("result.txt", "w")) == NULL){
+		puts("error on file...");
+		exit(1);
+	}
+	printf("printing on file\n");
+
+	for(i = 0; i < size; i++){
+		fprintf(fPtr, "%d ", array[i]);
+	}
+	fprintf(fPtr, "\n");
+
+	fclose(fPtr);
+}
+
+void printArray(int *array, int size){
+	int i;
+	for(i = 0; i < size; i++){
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
 
 // runs on device, called by host
 __global__ void gpuAdd(int *a, int *b, int *c){
-	*c = *a + *b;
+	c[threadIdx.x] = a[threadIdx.x]  + b[threadIdx.x];
 }
 
 int main(int argc, char **argv)
 {
-	int a, b, c; // host variables
+	int size = N * sizeof(int);
+	// host variables
+	int *a = (int *)malloc(size);
+	int *b = (int *)malloc(size);
+	int *c = (int *)malloc(size);
 	int *d_a, *d_b, *d_c; // device variables
+	int i;
+
 
 	// allocate device memory
-	cudaMalloc((void**)&d_a, sizeof(int));
-	cudaMalloc((void**)&d_b, sizeof(int));
-	cudaMalloc((void**)&d_c, sizeof(int));
+	cudaMalloc((void**)&d_a, size);
+	cudaMalloc((void**)&d_b, size);
+	cudaMalloc((void**)&d_c, size);
 
-	printf("insert first value: ");
-	scanf("%d", &a);
-	printf("insert second value: ");
-	scanf("%d", &b);
+	fillArray(a, N);
+	fillArray(b, N);
 
 	// copy inputs to device
-	cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
 	// kernel launch
 	puts("Launch kernel");
-	gpuAdd<<<1,1>>>(d_a, d_b, d_c);
+	// 1 block of 512 threads (1D threads vector)
+	gpuAdd<<<1,N>>>(d_a, d_b, d_c);
 
 	// copy output to host
-	cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+	printInFile(c, N);
 
-	printf("%d+%d=%d", a, b, c);
+	// free host memory
+	free(a);
+	free(b);
+	free(c);
 
 	// free device memory
 	cudaFree(d_a);
